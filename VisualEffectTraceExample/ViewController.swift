40bba0bab6d1400660136bdbb3af59440d19d8db
Fumiya Sakai <just1factory@gmail.com>
2019-04-20 17:39:19 +0900

    Initial Commit

---
+++ VisualEffectTraceExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisualEffectTraceExample
+//
+//  Created by 酒井文也 on 2019/04/20.
+//  Copyright © 2019 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
