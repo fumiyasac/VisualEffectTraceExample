a1b4cc88de4da07bc129e5ba8f98de1c9aa3db02
Fumiya Sakai <just1factory@gmail.com>
2020-02-12 01:48:40 +0900

    サンプルの総入れ替え

---
+++ VisualEffectTraceExample/VisualEffectTraceExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisualEffectTraceExample
+//
+//  Created by 酒井文也 on 2020/02/12.
+//  Copyright © 2020 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
