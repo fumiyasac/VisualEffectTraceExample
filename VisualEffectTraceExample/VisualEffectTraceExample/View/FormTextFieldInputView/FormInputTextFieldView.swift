2c98428e3e9d45b94ae130fe7d48ae567a6a24b6
Fumiya Sakai <just1factory@gmail.com>
2020-04-04 21:38:03 +0900

    [Application] FormInputTextFieldViewの実装をRxSwiftを加えてみる

--- VisualEffectTraceExample/VisualEffectTraceExample/View/FormTextFieldInputView/FormInputTextFieldView.swift
+++ VisualEffectTraceExample/VisualEffectTraceExample/View/FormTextFieldInputView/FormInputTextFieldView.swift
@@ -8,27 +8,32 @@
 
 import Foundation
 import UIKit
+import RxSwift
 
 // MARK: - Protocol
 
+// MEMO: View同士の処理の接続はProtocolを経由する方針とする
 protocol FormTextFieldInputViewDelegate: NSObjectProtocol {
 
     // MEMO: テキストフィールドの入力を変更した際の処理を画面へ伝える
-    func getInputTextByTextFieldType(_ text: String, style: FormInputTextFieldStyle)
+    func getInputTextByTextFieldType(_ text: String, formInputStyle: FormInputTextFieldStyle)
 }
 
 // MEMO: 利用しない場合やIBのエラーが出る場合は`@IBDesignable`をコメントする
 //@IBDesignable
 class FormTextFieldInputView: CustomViewBase {
 
+    private let disposeBag = DisposeBag()
+
     weak var delegate: FormTextFieldInputViewDelegate?
 
     // MARK: -  Variable
 
     // MEMO: 入力フィールドのタイプを格納する変数（デフォルト値を設定しておく）
-    private var selectedType: FormInputTextFieldStyle = .defaultTextInput
+    private var targetFormInputStyle: FormInputTextFieldStyle = .defaultTextInput
 
-    private var shouldSecure: Bool = true
+    // MEMO: 入力フィールドをSecure状態にするかどうかのフラグ値を格納する変数（デフォルト値を設定しておく）
+    private var shouldSecure: Bool = false
 
     // MARK: - @IBOutlet
 
@@ -78,44 +83,62 @@ class FormTextFieldInputView: CustomViewBase {
         inputTextField.placeholder = text
     }
 
-    func setFormInputTextFieldStyle(_ style: FormInputTextFieldStyle) {
-        displayShowPasswordButtonIfNeeded(style)
+    func setInitialText(_ text: String?) {
+        inputTextField.text = text
+    }
+
+    func setFormInputTextFieldStyle(_ formInputStyle: FormInputTextFieldStyle) {
+        displayShowPasswordButtonIfNeeded(formInputStyle)
     }
 
     // MARK: - Private Function
 
-    @objc func textFieldDidChange() {
+    private func displayShowPasswordButtonIfNeeded(_ formInputStyle: FormInputTextFieldStyle) {
 
-        if let targetText = inputTextField.text {
-            self.delegate?.getInputTextByTextFieldType(targetText, style: selectedType)
+        // MEMO: FormInputTextFieldStyleがパスワード入力の場合にはパスワード表示ボタンを非表示にする
+        let shouldDisplayShowPasswordButton = (formInputStyle == .secureTextInput)
+        if shouldDisplayShowPasswordButton {
+            shouldSecure = true
+            showPasswordButton.isHidden = false
+            showPasswordButtonWidthConstraint.constant = 33.0
+        } else {
+            shouldSecure = false
+            showPasswordButton.isHidden = true
+            showPasswordButtonWidthConstraint.constant = 0
         }
     }
 
-    @objc func showPasswordButtonTapped() {
+    private func showPasswordButtonTapped() {
 
         // MEMO: パスワード表示を見える状態にする
         shouldSecure = !shouldSecure
         inputTextField.isSecureTextEntry = !shouldSecure
     }
 
-    private func displayShowPasswordButtonIfNeeded(_ style: FormInputTextFieldStyle) {
-
-        // MEMO: FormInputTextFieldStyleがパスワード入力の場合にはパスワード表示ボタンを非表示にする
-        if style == .secureTextInput {
-            showPasswordButton.isHidden = false
-            showPasswordButtonWidthConstraint.constant = 33.0
-        } else {
-            showPasswordButton.isHidden = true
-            showPasswordButtonWidthConstraint.constant = 0
-        }
-    }
-
     private func setupFormTextFieldInputView() {
 
         // テキストフィールドの変化を検知した際のアクション設定
-        inputTextField.addTarget(self, action: #selector(self.textFieldDidChange), for: .editingChanged)
+        inputTextField.rx.text
+            .observeOn(MainScheduler.instance)
+            .subscribe(
+                onNext: { [weak self] targetText in
+                    guard let self = self else { return }
+                    if let targetText = self.inputTextField.text {
+                        self.delegate?.getInputTextByTextFieldType(targetText, formInputStyle: self.targetFormInputStyle)
+                    }
+                }
+            )
+            .disposed(by: disposeBag)
 
         // ボタン押下時のアクション設定
-        showPasswordButton.addTarget(self, action:  #selector(self.showPasswordButtonTapped), for: .touchUpInside)
+        showPasswordButton.rx.controlEvent(.touchUpInside)
+            .asDriver(onErrorJustReturn: ())
+            .drive(
+                onNext: { [weak self] _ in
+                    guard let self = self else { return }
+                    self.showPasswordButtonTapped()
+                }
+            )
+            .disposed(by: disposeBag)
     }
 }
