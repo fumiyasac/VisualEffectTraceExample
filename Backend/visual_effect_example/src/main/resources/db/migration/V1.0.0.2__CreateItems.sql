2468a830ddf77a9050769956a1a5e8c1850e782d
Fumiya Sakai <just1factory@gmail.com>
2020-07-12 16:24:20 +0900

    [Backend] Itemテーブルのカラムを変更する

--- Backend/visual_effect_example/src/main/resources/db/migration/V1.0.0.2__CreateItems.sql
+++ Backend/visual_effect_example/src/main/resources/db/migration/V1.0.0.2__CreateItems.sql
@@ -3,6 +3,7 @@ CREATE TABLE `items` (
   `title` varchar(255) NOT NULL COMMENT 'アイテムのタイトル表記',
   `statement` text COMMENT 'アイテムの概要本文表記',
   `thumbnail_url` text COMMENT 'アイテムのメイン画像サムネイルURL',
+  `category_id` int(11) NOT NULL COMMENT 'アイテムのカテゴリーID',
   `publish_flag` int(1) NOT NULL DEFAULT '0' COMMENT 'アイテム用公開フラグ（0:公開 1:非公開）',
   `announcement_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'アイテム公開日',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'アイテム作成日',
