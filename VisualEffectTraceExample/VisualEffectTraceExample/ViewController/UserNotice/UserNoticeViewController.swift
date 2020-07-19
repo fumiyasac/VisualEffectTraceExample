672000d3d19add9040758857ebf69a433c75a104
Fumiya Sakai <just1factory@gmail.com>
2020-07-19 11:59:19 +0900

    [Application] UserNoticeViewControllerに対応する画面を追加する

---
+++ VisualEffectTraceExample/VisualEffectTraceExample/ViewController/UserNotice/UserNoticeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserNoticeViewController.swift
+//  VisualEffectTraceExample
+//
+//  Created by 酒井文也 on 2020/07/19.
+//  Copyright © 2020 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class UserNoticeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
