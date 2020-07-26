6d4da87f53f92d1fbc463fdfee7bbc9f7c9b7419
Fumiya Sakai <just1factory@gmail.com>
2020-07-26 23:57:52 +0900

    [Backend] Swaggerへの追記

--- Backend/visual_effect_example/src/main/kotlin/net/just1factory/visual_effect_example/app/controller/api/v1/StoryController.kt
+++ Backend/visual_effect_example/src/main/kotlin/net/just1factory/visual_effect_example/app/controller/api/v1/StoryController.kt
@@ -1,13 +1,12 @@
 package net.just1factory.visual_effect_example.app.controller.api.v1
 
 // ドメイン層（Service）
+import io.swagger.annotations.*
 import net.just1factory.visual_effect_example.domain.service.StoryService
 
 // アプリケーション層（Request・Response）
 import net.just1factory.visual_effect_example.app.response.story.StoryListResponse
-
-// コンテキスト層（Exception）
-import net.just1factory.visual_effect_example.context.exception.NotFoundException
+import net.just1factory.visual_effect_example.app.response.topBanner.TopBannerListResponse
 
 // Spring Frameworkのインポート宣言
 import org.springframework.web.bind.annotation.GetMapping
@@ -16,10 +15,10 @@ import org.springframework.web.bind.annotation.RestController
 
 // MEMO: Setter Injectionを利用するためのアノテーション
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PathVariable
 
 @RestController
 @RequestMapping("/api/v1")
+@Api(description = "Story(アプリ内ではフォトギャラリー)画面表示用バナーに関するエンドポイント（表示コンテンツ）")
 class StoryController {
 
 	@Autowired
@@ -27,6 +26,12 @@ class StoryController {
 
 	// MEMO: StoryServiceを経由してStoryEntityにマッピングされたデータを全件取得する
 	@GetMapping("/story")
+	@ApiOperation(value = "Story一覧データ取得", produces = "application/json", notes = "DB内にあるStory一覧データ取得します。", response = TopBannerListResponse::class, authorizations = [Authorization(value = "発行したJWT")])
+	@ApiResponses(
+		value = [
+			ApiResponse(code = 200, message = "OK")
+		]
+	)
 	fun findAll(): StoryListResponse {
 		val stories = storyService.findAll()
 		return StoryListResponse(result = stories)
