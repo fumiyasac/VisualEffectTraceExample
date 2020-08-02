85ca8dddd7f6085e2c0080291ef5bb49f339dc7e
Fumiya Sakai <just1factory@gmail.com>
2020-08-02 12:20:56 +0900

    [Application & Backend] 諸々不要そうなものを削除

--- Backend/visual_effect_example/src/main/resources/db/migration/V1.0.0.4__CreateItemComments.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `item_comments` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) unsigned NOT NULL COMMENT 'ユーザーID',
-  `item_id` int(11) unsigned NOT NULL COMMENT 'アイテムID',
-  `comment` text COMMENT 'アイテムへのコメント表記',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'アイテムへのコメント作成日',
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'アイテムへのコメント更新日',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
