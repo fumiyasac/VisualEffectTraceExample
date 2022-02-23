4e366a82ef320f30cd74a524937f6513bace99e8
Fumiya Sakai <just1factory@gmail.com>
2022-02-23 19:12:12 +0900

    iOS15以降でのUITabBarとUINavigationControllerのAppearance設定を追加

--- VisualEffectTraceExample/VisualEffectTraceExample/AppDelegate.swift
+++ VisualEffectTraceExample/VisualEffectTraceExample/AppDelegate.swift
@@ -27,7 +27,60 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let domainDependency = DependenciesDefinition()
         domainDependency.inject()
 
+        // MEMO: iOS15以降で適用するUINavigationController/UITabBarControllerの見た目に関する設定を適用する
+        setupNavigationBarAppearance()
+        setupTabBarAppearance()
+
         return true
+    }
+
+    // MARK: - Private Function
+
+    private func setupNavigationBarAppearance() {
+
+        // iOS15以降ではUINavigationBarの配色指定方法が変化する点に注意する
+        // https://shtnkgm.com/blog/2021-08-18-ios15.html
+
+        if #available(iOS 15.0, *) {
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.configureWithOpaqueBackground()
+
+            // MEMO: UINavigationBar内部におけるタイトル文字の装飾設定
+            navigationBarAppearance.titleTextAttributes = [
+                NSAttributedString.Key.font : UIFont(name: "HelveticaNeue-Bold", size: 14.0)!,
+                NSAttributedString.Key.foregroundColor : UIColor.white
+            ]
+
+            // MEMO: UINavigationBar背景色の装飾設定
+            navigationBarAppearance.backgroundColor = UIColor.systemYellow
+
+            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        }
+    }
+
+    private func setupTabBarAppearance() {
+
+        // iOS15以降でiOS14以前と同じUINavigationBarの配色指定方法が変化する点に注意する
+        // https://shtnkgm.com/blog/2021-08-18-ios15.html
+
+        if #available(iOS 15.0, *) {
+            let tabBarAppearance = UITabBarAppearance()
+            let tabBarItemAppearance = UITabBarItemAppearance()
+
+            // UITabBarItemの選択時と非選択時の文字色の装飾設定
+            tabBarItemAppearance.normal.titleTextAttributes = [
+                NSAttributedString.Key.foregroundColor : UIColor.lightGray
+                
+            ]
+            tabBarItemAppearance.selected.titleTextAttributes = [
+                NSAttributedString.Key.foregroundColor : UIColor(code: "#cda966")
+            ]
+            tabBarAppearance.stackedLayoutAppearance = tabBarItemAppearance
+
+            UITabBar.appearance().standardAppearance = tabBarAppearance
+            UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        }
     }
 
     // MARK: UISceneSession Lifecycle
