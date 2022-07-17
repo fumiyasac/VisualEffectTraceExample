39c09624cfc584c94eb25c65955e347ebbda73e2
Fumiya Sakai <just1factory@gmail.com>
2022-07-17 14:13:37 +0900

    MainScreenRepositoryに関するテストケースを新規追加する

--- VisualEffectTraceExample/VisualEffectTraceExampleTests/Domain/Repository/MainScreenRepositorySpec.swift
+++ VisualEffectTraceExample/VisualEffectTraceExampleTests/Domain/Repository/MainScreenRepositorySpec.swift
@@ -6,31 +6,183 @@
 //  Copyright © 2022 酒井文也. All rights reserved.
 //
 
+@testable import VisualEffectTraceExample
+
+import Nimble
+import Quick
+import RxBlocking
+import RxSwift
+import SwiftyMocky
 import XCTest
 
-class MainScreenRepositorySpec: QuickSpec {
+final class MainScreenRepositorySpec: QuickSpec {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    // MARK: - Override
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    override func spec() {
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+        // MEMO: Testで動かす想定のDIコンテナのインスタンスを生成する
+        let testingDependency = DependenciesDefinition()
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let realmAccessManager = RealmAccessProtocolMock()
+        let keychainAccessManager = KeychainAccessProtocolMock()
+        let target = MainScreenRepository()
+
+        describe("MainScreenRepository") {
+
+            // MARK: - searchApplicationUserDataを実行した際のテスト
+
+            describe("#searchApplicationUserData") {
+
+                // MEMO: チュートリアル画面を表示する想定
+                context("チュートリアル画面へ遷移する場合") {
+                    let applicationUserEntity = ApplicationUserEntity()
+                    // Mockに差し替えたメソッドが返却する値を定める
+                    beforeEach {
+                        testingDependency.injectIndividualMock(
+                            mockInstance: realmAccessManager,
+                            protocolName: RealmAccessProtocol.self
+                        )
+                        testingDependency.injectIndividualMock(
+                            mockInstance: keychainAccessManager,
+                            protocolName: KeychainAccessProtocol.self
+                        )
+                        realmAccessManager.given(
+                            .getApplicationUser(
+                                willReturn: applicationUserEntity
+                            )
+                        )
+                        keychainAccessManager.given(
+                            .existsJsonAccessToken(
+                                willReturn: false
+                            )
+                        )
+                    }
+                    afterEach {
+                        testingDependency.removeIndividualMock(
+                            protocolName: RealmAccessProtocol.self
+                        )
+                        testingDependency.removeIndividualMock(
+                            protocolName: KeychainAccessProtocol.self
+                        )
+                    }
+                    it("ApplicationUserStatus.needToMoveTutorialScreenを返却すること") {
+                        expect(target.searchApplicationUserData()).to(equal(ApplicationUserStatus.needToMoveTutorialScreen))
+                    }
+                }
+
+                // MEMO: サインイン画面を表示する想定
+                context("サインイン画面へ遷移する場合") {
+                    let applicationUserEntity = ApplicationUserEntity()
+                    applicationUserEntity.alreadyPassTutorial = true
+                    // Mockに差し替えたメソッドが返却する値を定める
+                    beforeEach {
+                        testingDependency.injectIndividualMock(
+                            mockInstance: realmAccessManager,
+                            protocolName: RealmAccessProtocol.self
+                        )
+                        testingDependency.injectIndividualMock(
+                            mockInstance: keychainAccessManager,
+                            protocolName: KeychainAccessProtocol.self
+                        )
+                        realmAccessManager.given(
+                            .getApplicationUser(
+                                willReturn: applicationUserEntity
+                            )
+                        )
+                        keychainAccessManager.given(
+                            .existsJsonAccessToken(
+                                willReturn: false
+                            )
+                        )
+                    }
+                    afterEach {
+                        testingDependency.removeIndividualMock(
+                            protocolName: RealmAccessProtocol.self
+                        )
+                        testingDependency.removeIndividualMock(
+                            protocolName: KeychainAccessProtocol.self
+                        )
+                    }
+                    it("ApplicationUserStatus.needToMoveSigninScreenを返却すること") {
+                        expect(target.searchApplicationUserData()).to(equal(ApplicationUserStatus.needToMoveSigninScreen))
+                    }
+                }
+
+                // MEMO: メインのタブバー画面を表示する想定
+                context("メインのタブバー画面へ遷移する場合") {
+                    let applicationUserEntity = ApplicationUserEntity()
+                    applicationUserEntity.alreadyPassTutorial = true
+                    // Mockに差し替えたメソッドが返却する値を定める
+                    beforeEach {
+                        testingDependency.injectIndividualMock(
+                            mockInstance: realmAccessManager,
+                            protocolName: RealmAccessProtocol.self
+                        )
+                        testingDependency.injectIndividualMock(
+                            mockInstance: keychainAccessManager,
+                            protocolName: KeychainAccessProtocol.self
+                        )
+                        realmAccessManager.given(
+                            .getApplicationUser(
+                                willReturn: applicationUserEntity
+                            )
+                        )
+                        keychainAccessManager.given(
+                            .existsJsonAccessToken(
+                                willReturn: true
+                            )
+                        )
+                    }
+                    afterEach {
+                        testingDependency.removeIndividualMock(
+                            protocolName: RealmAccessProtocol.self
+                        )
+                        testingDependency.removeIndividualMock(
+                            protocolName: KeychainAccessProtocol.self
+                        )
+                    }
+                    it("ApplicationUserStatus.needToMoveGlobalTabBarScreenを返却すること") {
+                        expect(target.searchApplicationUserData()).to(equal(ApplicationUserStatus.needToMoveGlobalTabBarScreen))
+                    }
+                }
+
+                // MEMO: アプリを初回起動した場合にチュートリアル画面を表示する想定
+                context("アプリを初回起動した場合にチュートリアル画面へ遷移する場合") {
+                    // Mockに差し替えたメソッドが返却する値を定める
+                    beforeEach {
+                        testingDependency.injectIndividualMock(
+                            mockInstance: realmAccessManager,
+                            protocolName: RealmAccessProtocol.self
+                        )
+                        testingDependency.injectIndividualMock(
+                            mockInstance: keychainAccessManager,
+                            protocolName: KeychainAccessProtocol.self
+                        )
+                        realmAccessManager.given(
+                            .getApplicationUser(
+                                willReturn: nil
+                            )
+                        )
+                        keychainAccessManager.given(
+                            .existsJsonAccessToken(
+                                willReturn: false
+                            )
+                        )
+                    }
+                    afterEach {
+                        testingDependency.removeIndividualMock(
+                            protocolName: RealmAccessProtocol.self
+                        )
+                        testingDependency.removeIndividualMock(
+                            protocolName: KeychainAccessProtocol.self
+                        )
+                    }
+                    it("ApplicationUserStatus.needToMoveTutorialScreenを返却すること") {
+                        expect(target.searchApplicationUserData()).to(equal(ApplicationUserStatus.needToMoveTutorialScreen))
+                    }
+                }
+            }
         }
     }
-
 }
