1a87a5ff483970ab57dce2c29d502fceadb2c960
Fumiya Sakai <just1factory@gmail.com>
2022-07-18 21:08:53 +0900

    AnnouncementDetailAPIResponseをEquatableに準拠させる＆initializerを追加する対応

--- VisualEffectTraceExample/VisualEffectTraceExample/Domain/DataTransfer/Response/AnnouncementDetailAPIResponse.swift
+++ VisualEffectTraceExample/VisualEffectTraceExample/Domain/DataTransfer/Response/AnnouncementDetailAPIResponse.swift
@@ -11,7 +11,7 @@ import Foundation
 // MEMO: お知らせ詳細表示用のAPIレスポンス定義
 // ※ ここではアイテム表示画面のお知らせ表示セクションとお知らせ詳細画面で利用する
 
-struct AnnouncementDetailAPIResponse: Decodable {
+struct AnnouncementDetailAPIResponse: Decodable, Equatable {
 
     let result: AnnouncementEntity
 
@@ -23,9 +23,21 @@ struct AnnouncementDetailAPIResponse: Decodable {
 
     // MARK: - Initializer
 
+    init(result: AnnouncementEntity) {
+        self.result = result
+    }
+
     // JSONの配列内の要素を取得する → JSONの配列内の要素にある値をDecodeして初期化する
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: Keys.self)
         self.result = try container.decode(AnnouncementEntity.self, forKey: .result)
+    }
+
+    // MARK: - Equatable
+
+    // MEMO: Equatableプロトコルに適合させるための処理
+
+    static func == (lhs: AnnouncementDetailAPIResponse, rhs: AnnouncementDetailAPIResponse) -> Bool {
+        return lhs.result == rhs.result
     }
 }
