c8f398759e3a730156319ce42fa756dbbbf99255
Fumiya Sakai <just1factory@gmail.com>
2022-07-30 23:15:47 +0900

    StoryViewModelSpecを新規作成

---
+++ VisualEffectTraceExample/VisualEffectTraceExampleTests/ViewModel/StoryViewModelSpec.swift
@@ -0,0 +1,20 @@
+//
+//  StoryViewModelSpec.swift
+//  VisualEffectTraceExampleTests
+//
+//  Created by 酒井文也 on 2022/07/30.
+//  Copyright © 2022 酒井文也. All rights reserved.
+//
+
+@testable import VisualEffectTraceExample
+
+import Nimble
+import Quick
+import RxBlocking
+import RxSwift
+import SwiftyMocky
+import XCTest
+
+final class StoryViewModelSpec: QuickSpec {
+
+}
