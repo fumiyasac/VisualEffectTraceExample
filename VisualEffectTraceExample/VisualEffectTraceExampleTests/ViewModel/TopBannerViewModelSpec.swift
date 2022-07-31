bede7f28206f2cc2afc18bc2e9d7ff4e44ba3cdd
Fumiya Sakai <just1factory@gmail.com>
2022-07-31 17:58:23 +0900

    TopBannerViewModelSpecを新規作成

---
+++ VisualEffectTraceExample/VisualEffectTraceExampleTests/ViewModel/TopBannerViewModelSpec.swift
@@ -0,0 +1,20 @@
+//
+//  TopBannerViewModelSpec.swift
+//  VisualEffectTraceExampleTests
+//
+//  Created by 酒井文也 on 2022/07/31.
+//  Copyright © 2022 酒井文也. All rights reserved.
+//
+
+@testable import VisualEffectTraceExample
+
+import Nimble
+import Quick
+import RxBlocking
+import RxSwift
+import SwiftyMocky
+import XCTest
+
+final class TopBannerViewModelSpec: QuickSpec {
+
+}
