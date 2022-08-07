471166480b61d36b9a753db0e12453a620047356
Fumiya Sakai <just1factory@gmail.com>
2022-08-07 13:47:49 +0900

    TopBannerViewModelSpecの追加対応

--- VisualEffectTraceExample/VisualEffectTraceExampleTests/ViewModel/TopBannerViewModelSpec.swift
+++ VisualEffectTraceExample/VisualEffectTraceExampleTests/ViewModel/TopBannerViewModelSpec.swift
@@ -17,4 +17,156 @@ import XCTest
 
 final class TopBannerViewModelSpec: QuickSpec {
 
+    // MARK: - Override
+
+    // MEMO: ViewModelクラス内のInput&Outputの変化が検知できていることを確認する
+    override func spec() {
+
+        // MEMO: Testで動かす想定のDIコンテナのインスタンスを生成する
+        let testingDependency = DependenciesDefinition()
+
+        let topBannerUseCase = TopBannerUseCaseMock()
+
+        // MARK: - initialFetchTriggerを実行した際のテスト
+
+        // MEMO: サーバーから表示内容を取得する場合
+        describe("#initialFetchTrigger") {
+            context("サーバーからの取得処理が成功した場合") {
+                let topBannerAPIResponse = getTopBannerAPIResponse()
+
+                // Mockに差し替えたメソッドが返却する値を定める
+                beforeEach {
+                    testingDependency.injectIndividualMock(
+                        mockInstance: topBannerUseCase,
+                        protocolName: TopBannerUseCase.self
+                    )
+                    topBannerUseCase.given(
+                        .execute(
+                            willReturn: Single.just(topBannerAPIResponse)
+                        )
+                    )
+                }
+                afterEach {
+                    testingDependency.removeIndividualMock(
+                        protocolName: TopBannerUseCase.self
+                    )
+                }
+                it("viewModel.outputs.topBannerItemsが取得データと一致する＆viewModel.outputs.requestStatusがAPIRequestState.successとなること") {
+                    let target = TopBannerViewModel()
+                    target.inputs.initialFetchTrigger.onNext(())
+                    expect(try! target.outputs.topBannerItems.toBlocking().first()).to(equal(topBannerAPIResponse.result))
+                    expect(try! target.outputs.requestStatus.toBlocking().first()).to(equal(APIRequestState.success))
+                }
+            }
+            context("サーバーへの登録処理が失敗した場合") {
+                beforeEach {
+                    testingDependency.injectIndividualMock(
+                        mockInstance: topBannerUseCase,
+                        protocolName: TopBannerUseCase.self
+                    )
+                    topBannerUseCase.given(
+                        .execute(
+                            willReturn: Single.error(CommonError.invalidResponse("データの取得に失敗しました。"))
+                        )
+                    )
+                }
+                afterEach {
+                    testingDependency.removeIndividualMock(
+                        protocolName: TopBannerUseCase.self
+                    )
+                }
+                it("viewModel.outputs.topBannerItemsが取得データが空配列＆viewModel.outputs.requestStatusがAPIRequestState.errorとなること") {
+                    let target = TopBannerViewModel()
+                    target.inputs.initialFetchTrigger.onNext(())
+                    expect(try! target.outputs.topBannerItems.toBlocking().first()).to(equal([]))
+                    expect(try! target.outputs.requestStatus.toBlocking().first()).to(equal(APIRequestState.error))
+                }
+            }
+        }
+
+        // MARK: - pullToRefreshTriggerを実行した際のテスト
+
+        // MEMO: PullToRefreshでサーバーから表示内容を取得する場合
+        describe("#pullToRefreshTrigger") {
+            context("サーバーからの取得処理が成功した場合") {
+                let topBannerAPIResponse = getTopBannerAPIResponse()
+
+                // Mockに差し替えたメソッドが返却する値を定める
+                beforeEach {
+                    testingDependency.injectIndividualMock(
+                        mockInstance: topBannerUseCase,
+                        protocolName: TopBannerUseCase.self
+                    )
+                    topBannerUseCase.given(
+                        .execute(
+                            willReturn: Single.just(topBannerAPIResponse)
+                        )
+                    )
+                }
+                afterEach {
+                    testingDependency.removeIndividualMock(
+                        protocolName: TopBannerUseCase.self
+                    )
+                }
+                it("viewModel.outputs.topBannerItemsが取得データと一致する＆viewModel.outputs.requestStatusがAPIRequestState.successとなること") {
+                    let target = TopBannerViewModel()
+                    target.inputs.pullToRefreshTrigger.onNext(())
+                    expect(try! target.outputs.topBannerItems.toBlocking().first()).to(equal(topBannerAPIResponse.result))
+                    expect(try! target.outputs.requestStatus.toBlocking().first()).to(equal(APIRequestState.success))
+                }
+            }
+            context("サーバーへの登録処理が失敗した場合") {
+                beforeEach {
+                    testingDependency.injectIndividualMock(
+                        mockInstance: topBannerUseCase,
+                        protocolName: TopBannerUseCase.self
+                    )
+                    topBannerUseCase.given(
+                        .execute(
+                            willReturn: Single.error(CommonError.invalidResponse("データの取得に失敗しました。"))
+                        )
+                    )
+                }
+                afterEach {
+                    testingDependency.removeIndividualMock(
+                        protocolName: TopBannerUseCase.self
+                    )
+                }
+                it("viewModel.outputs.topBannerItemsが取得データが空配列＆viewModel.outputs.requestStatusがAPIRequestState.errorとなること") {
+                    let target = TopBannerViewModel()
+                    target.inputs.pullToRefreshTrigger.onNext(())
+                    expect(try! target.outputs.topBannerItems.toBlocking().first()).to(equal([]))
+                    expect(try! target.outputs.requestStatus.toBlocking().first()).to(equal(APIRequestState.error))
+                }
+            }
+        }
+
+        // MARK: - undoAPIRequestStateTriggerを実行した際のテスト
+
+        // MEMO: APIRequestStateを元に戻す場合
+        describe("#undoAPIRequestStateTrigger") {
+            context("APIリクエスト結果ダイアログ表示後に画面状態を元に戻す場合") {
+                it("viewModel.outputs.requestStatusがAPIRequestState.noneとなること") {
+                    let target = TopBannerViewModel()
+                    target.inputs.undoAPIRequestStateTrigger.onNext(())
+                    expect(try! target.outputs.requestStatus.toBlocking().first()).to(equal(APIRequestState.none))
+                }
+            }
+        }
+    }
+
+    private func getTopBannerAPIResponse() -> TopBannerAPIResponse {
+
+        // JSONファイルから表示用のデータを取得する
+        guard let path = Bundle(for: type(of: self)).path(forResource: "top_banner_data", ofType: "json") else {
+            fatalError()
+        }
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: path)) else {
+            fatalError()
+        }
+        guard let topBannerAPIResponse = try? JSONDecoder().decode(TopBannerAPIResponse.self, from: data) else {
+            fatalError()
+        }
+        return topBannerAPIResponse
+    }
 }
