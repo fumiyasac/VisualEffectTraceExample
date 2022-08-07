396ea7adb8772c45a53626fe60d71b2ae037174b
Fumiya Sakai <just1factory@gmail.com>
2022-08-07 11:40:35 +0900

    StoryViewModelSpecの実装

--- VisualEffectTraceExample/VisualEffectTraceExampleTests/ViewModel/StoryViewModelSpec.swift
+++ VisualEffectTraceExample/VisualEffectTraceExampleTests/ViewModel/StoryViewModelSpec.swift
@@ -17,4 +17,104 @@ import XCTest
 
 final class StoryViewModelSpec: QuickSpec {
 
+    // MARK: - Override
+
+    // MEMO: ViewModelクラス内のInput&Outputの変化が検知できていることを確認する
+    override func spec() {
+
+        // MEMO: Testで動かす想定のDIコンテナのインスタンスを生成する
+        let testingDependency = DependenciesDefinition()
+
+        let storyUseCase = StoryUseCaseMock()
+
+        // MARK: - initialFetchTriggerを実行した際のテスト
+
+        // MEMO: サーバーから表示内容を取得する場合
+        describe("#initialFetchTrigger") {
+            context("サーバーからの取得処理が成功した場合") {
+                let storyAPIResponse = getStoryAPIResponse()
+
+                // Mockに差し替えたメソッドが返却する値を定める
+                beforeEach {
+                    testingDependency.injectIndividualMock(
+                        mockInstance: storyUseCase,
+                        protocolName: StoryUseCase.self
+                    )
+                    storyUseCase.given(
+                        .execute(
+                            willReturn: Single.just(storyAPIResponse)
+                        )
+                    )
+                }
+                afterEach {
+                    testingDependency.removeIndividualMock(
+                        protocolName: StoryUseCase.self
+                    )
+                }
+                it("viewModel.outputs.storyItemsが取得データと一致する＆viewModel.outputs.requestStatusがAPIRequestState.successとなること") {
+                    let target = StoryViewModel()
+                    target.inputs.initialFetchTrigger.onNext(())
+                    expect(try! target.outputs.storyItems.toBlocking().first()).to(equal(storyAPIResponse.result))
+                    expect(try! target.outputs.requestStatus.toBlocking().first()).to(equal(APIRequestState.success))
+                }
+            }
+            context("サーバーへの登録処理が失敗した場合") {
+                beforeEach {
+                    testingDependency.injectIndividualMock(
+                        mockInstance: storyUseCase,
+                        protocolName: StoryUseCase.self
+                    )
+                    storyUseCase.given(
+                        .execute(
+                            willReturn: Single.error(CommonError.invalidResponse("データの取得に失敗しました。"))
+                        )
+                    )
+                }
+                afterEach {
+                    testingDependency.removeIndividualMock(
+                        protocolName: StoryUseCase.self
+                    )
+                }
+                afterEach {
+                    testingDependency.removeIndividualMock(
+                        protocolName: StoryUseCase.self
+                    )
+                }
+                it("viewModel.outputs.storyItemsが取得データが空配列＆viewModel.outputs.requestStatusがAPIRequestState.errorとなること") {
+                    let target = StoryViewModel()
+                    target.inputs.initialFetchTrigger.onNext(())
+                    expect(try! target.outputs.storyItems.toBlocking().first()).to(equal([]))
+                    expect(try! target.outputs.requestStatus.toBlocking().first()).to(equal(APIRequestState.error))
+                }
+            }
+        }
+
+        // MARK: - undoAPIRequestStateTriggerを実行した際のテスト
+
+        // MEMO: APIRequestStateを元に戻す場合
+        describe("#undoAPIRequestStateTrigger") {
+            context("APIリクエスト結果ダイアログ表示後に画面状態を元に戻す場合") {
+                it("viewModel.outputs.requestStatusがAPIRequestState.noneとなること") {
+                    let target = StoryViewModel()
+                    target.inputs.undoAPIRequestStateTrigger.onNext(())
+                    expect(try! target.outputs.requestStatus.toBlocking().first()).to(equal(APIRequestState.none))
+                }
+            }
+        }
+    }
+
+    private func getStoryAPIResponse() -> StoryAPIResponse {
+
+        // JSONファイルから表示用のデータを取得する
+        guard let path = Bundle(for: type(of: self)).path(forResource: "story_data", ofType: "json") else {
+            fatalError()
+        }
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: path)) else {
+            fatalError()
+        }
+        guard let storyAPIResponse = try? JSONDecoder().decode(StoryAPIResponse.self, from: data) else {
+            fatalError()
+        }
+        return storyAPIResponse
+    }
 }
